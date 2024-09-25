521507d467720fe56f4d9055b9433946b3ca5412
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-09-24 23:35:03 -0700

    formatted the document for a cleaner appearance

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
-import fs from 'fs';
-import generateMarkdown from './utils/generateMarkdown.js';
-import { type } from "os";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
 
 const questions = [
-    {
-        name: 'title',
-        type: 'input',
-        message: 'What would you like to title your README?',
-    },
-    {
-        name: 'installation',
-        type: 'input',
-        message: 'How do you install this project?',
-    },
-    {
-        name: 'description',
-        type: 'input',
-        message: 'Describe your project.',
-    },
-    {
-        name: 'usage',
-        type: 'input',
-        message: 'What is the intended use of this project?',
-    },
-    {
-        name: 'credits',
-        type: 'input',
-        message: 'Take a moment to Credit the authors or document sites used for this project.',
-    },
-    {
-        name: 'github',
-        type: 'input',
-        message: 'Please provide your GitHub name for people to reach out too for any questions.'
-    },
-    {
-        name: 'email',
-        type: 'input',
-        message: 'Please provide an email for anyone to reachout to for any questions.'
-    },
-    {
-        name: 'license',
-        type: 'list',
-        message: 'What licenses were used for this project?',
-        choices: ['ISC', 'MIT', 'Apache-2.0'],
-    },
+  {
+    name: "title",
+    type: "input",
+    message: "What would you like to title your README?",
+  },
+  {
+    name: "installation",
+    type: "input",
+    message: "How do you install this project?",
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "Describe your project.",
+  },
+  {
+    name: "usage",
+    type: "input",
+    message: "What is the intended use of this project?",
+  },
+  {
+    name: "credits",
+    type: "input",
+    message:
+      "Take a moment to Credit the authors or document sites used for this project.",
+  },
+  {
+    name: "github",
+    type: "input",
+    message:
+      "Please provide your GitHub name for people to reach out too for any questions.",
+  },
+  {
+    name: "email",
+    type: "input",
+    message:
+      "Please provide an email for anyone to reachout to for any questions.",
+  },
+  {
+    name: "license",
+    type: "list",
+    message: "What licenses were used for this project?",
+    choices: ["ISC", "MIT", "Apache-2.0"],
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
-    const fileName = data.title
-    fs.writeFile(`${fileName}.md`, generateMarkdown(data), (err) =>
-        err ? console.error(err) : console.log('Success!')
-    )
+  const fileName = data.title;
+  fs.writeFile(`${fileName}.md`, generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-    .prompt(questions)
-    .then(data => writeToFile(data))
+  inquirer.prompt(questions).then((data) => writeToFile(data));
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license === 'ISC') {
-    return '![GitHub license](https://img.shields.io/badge/license-ISC-blue.svg)';
-  } else if(license === 'MIT') {
-    return '![GitHub license](https://img.shields.io/badge/license-MIT-green.svg)';
-  } else if(license === 'Apache-2.0') {
-    return '![GitHub license](https://img.shields.io/badge/license-Apache-red.svg)';
+  if (license === "ISC") {
+    return "![GitHub license](https://img.shields.io/badge/license-ISC-blue.svg)";
+  } else if (license === "MIT") {
+    return "![GitHub license](https://img.shields.io/badge/license-MIT-green.svg)";
+  } else if (license === "Apache-2.0") {
+    return "![GitHub license](https://img.shields.io/badge/license-Apache-red.svg)";
   }
   return renderLicenseLink(license);
 }
@@ -14,22 +14,22 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license === 'ISC') {
+  if (license === "ISC") {
     return `![License](https://opensource.org/licenses/${license})`;
-  } else if(license === 'MIT') {
+  } else if (license === "MIT") {
     return `![License](https://opensource.org/licenses/${license})`;
-  } else if(license === 'Apache-2.0') {
+  } else if (license === "Apache-2.0") {
     return `![License](https://opensource.org/licenses/${license})`;
   } else {
-    return '';
+    return "";
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(!license) {
-    return '';
+  if (!license) {
+    return "";
   }
   return `This project is licensed under ${license}`;
 }
